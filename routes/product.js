2b2765bd2295fbb5d9dfc808b82d91c5ca0bfb3f
Pravesh <pravesh_c@me.iitr.ac.in>
2021-11-03 10:58:17 +0530

    add product route added

--- routes/product.js
+++ routes/product.js
@@ -11,4 +11,32 @@ router.get('/',async(req,res)=>{
  }
 });
 
+router.post('/add',async(req,res)=>{
+    try{
+        const product= await new productModel({
+            brand:req.body.brand,
+            size:req.body.size,
+            gender:req.body.gender,
+            category:req.body.category
+        });
+
+        await product.save().then((a)=>{
+            res.status(201).json(a);
+
+            console.log("Product Added");
+        });
+
+    }catch(err){
+        res.send('Error'+err);
+    }
+})
+
+router.post('/search',async(req,res)=>{
+    const brands= await req.body.brands;
+    const sizes= await req.body.sizes;
+    const gender = await req.body.sizes;
+    const categories = await req.body.categories;
+
+})
+
 module.exports=router;
